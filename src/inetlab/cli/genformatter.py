3f2ae6e82f91b4a63eb45e812ed193df27e96222
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2021-09-10 23:57:20 -0400

    Cleanup; made compatible with wide Unicode chars and terminal colors

--- src/inetlab/cli/genformatter.py
+++ src/inetlab/cli/genformatter.py
@@ -1,7 +1,8 @@
 import os, sys
-from pprint import pprint
+import re
 
-class GenericFormatter(object) :
+
+class GenericFormatter :
     def __init__ (self, format_str) :
         opts = format_str.split(',')
         pars = {}
@@ -26,8 +27,9 @@ class GenericFormatter(object) :
 
     def close(self) :
         return self._formatter.close()
-            
-class CSVFormatter(object) :
+
+
+class CSVFormatter :
     def __init__ (self,pars) :
         import csv
         self.out = csv.writer(sys.stdout)
@@ -35,12 +37,10 @@ class CSVFormatter(object) :
         self.out.writerow(row)
     def close(self) :
         pass
-    
-    
-    
-class AlignedFormatter(object) :
+
+
+class AlignedFormatter :
     def __init__ (self, pars) :
-#        pprint(pars)
         self.rows = []
         self.alignchar = '|'
         self.alignchar1 = '+'
@@ -59,39 +59,47 @@ class AlignedFormatter(object) :
         self.rows.append(row)
         
     def close(self) :
-        def getcol(row,idx) :
-            x=row[idx]
-            if type(x)==tuple :
-                x=x[1]
+        def getcol(row,irow) :
+            x = row[irow]
+            if type(x) == tuple :
+                x = x[1]
             if not isinstance(x, str) :
-                x=str(x)
+                x = str(x)
             return x
-        def getcolfmt(row,idx) :
-            x=row[idx]
-            fmt=None
-            if type(x)==tuple :
-                x=x[1]
-                fmt=x[0]
+        def getcolfmt(row,irow) :
+            x = row[irow]
+            fmt = None
+            if type(x) == tuple :
+                x = x[1]
+                fmt = x[0]
             if not isinstance(x, str) :
-                x=str(x)
+                x = str(x)
             if fmt is not None :
-                x=fmt(x)
+                x = fmt(x)
             return x
-        def getfmt(row,idx) :
-            x=row[idx]
-            if type(x)==tuple :
+        def getfmt(row,irow) :
+            x = row[irow]
+            if type(x) == tuple :
                 return x[0]
             else :
                 return lambda y : y
+
+        def ulen(s) :
+            s = re.compile(r'\x1b.+?m').sub('', s)
+            try :
+                from wcwidth import wcswidth
+                return wcswidth(s)
+            except ModuleNotFoundError :
+                return len(s)
 
         if not self.rows :
             return
         colw = []
         for idx in range(len(self.rows[0])) :
-            colw.append(max(len(getcol(row,idx)) for row in self.rows))
+            colw.append(max(ulen(getcol(row,idx)) for row in self.rows))
         if self.width is not None :
             for idx in range(len(colw)) :
-                if idx >0 and colw[idx] > self.width :
+                if idx > 0 and colw[idx] > self.width :
                     colw[idx] = self.width
 
         if self.fill_last :
@@ -103,14 +111,15 @@ class AlignedFormatter(object) :
             for idx in range(len(row)) :
                 s = getcol(row,idx)
                 f = getfmt(row,idx)
-                if len(s) <= colw[idx] :
-                    s = s + " " * (colw[idx] - len(s))
+                if ulen(s) <= colw[idx] :
+                    s = s + " " * (colw[idx] - ulen(s))
                 else :
+                    # FIXME: this must be re-written for wide unicode chars
                     s = s[:colw[idx] - len(self.cont)] + self.cont
                 res.append(f(s))
             print((' ' + self.alignchar + ' ').join(res))
             if self.header is not None and self.header == i_row :
                 res = []
                 for idx in range(len(row)) :
-                    res.append("-" * ((1 if idx==0 else 2)+colw[idx]))
+                    res.append("-" * ((1 if idx == 0 else 2)+colw[idx]))
                 print(self.alignchar1.join(res))
