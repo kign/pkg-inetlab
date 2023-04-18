cbdc8ede4af781e4a518a7068e05c0720be57611
Konstantin Ignatiev <kignatiev@avaya.com>
2023-04-17 21:50:12 -0400

    Fixed config.py

--- src/inetlab/cli/config.py
+++ src/inetlab/cli/config.py
@@ -1,4 +1,5 @@
-import os, re
+import os, re, inspect
 
 def config(name, default=None, glob=False) :
-    return os.environ.get((("GLOBAL" if glob else re.compile(r'[^0-9a-z_]+', re.I).sub('_', os.path.basename(__file__))) + '_' + name).upper(), default)
+    prefix = "GLOBAL" if glob else re.compile(r'[^0-9a-z_]+', re.I).sub('_', os.path.basename(inspect.stack()[1].filename))
+    return os.environ.get((prefix + '_' + name).upper(), default)
